06089298d414a141e77b6bf0de5df89d48c0b1ed
pablo.rodriguez.mier <pablo.rodriguez.mier@usc.es>
2013-12-18 11:08:36 +0100

    ComposIT support to work with iServe OperationDiscoverer

--- src/main/java/es/usc/citius/composit/iserve/Main.java
+++ src/main/java/es/usc/citius/composit/iserve/Main.java
@@ -9,12 +9,15 @@ import es.usc.citius.composit.core.composition.search.CompositionProblem;
 import es.usc.citius.composit.core.matcher.graph.MatchGraph;
 import es.usc.citius.composit.core.model.impl.SignatureIO;
 import es.usc.citius.composit.iserve.discovery.iServeMatchGraphBasedDiscoverer;
+import es.usc.citius.composit.iserve.discovery.iServeOperationDiscovererAdapter;
 import es.usc.citius.composit.iserve.match.iServePluginKBMatchGraph;
 import es.usc.citius.composit.iserve.util.WSCImportUtils;
 import es.usc.citius.composit.wsc08.data.WSCTest;
 import uk.ac.open.kmi.iserve.api.iServeEngine;
 import uk.ac.open.kmi.iserve.api.iServeEngineFactory;
+import uk.ac.open.kmi.iserve.discovery.api.ConceptMatcher;
 import uk.ac.open.kmi.iserve.discovery.disco.LogicConceptMatchType;
+import uk.ac.open.kmi.iserve.discovery.disco.impl.GenericLogicDiscoverer;
 import uk.ac.open.kmi.iserve.sal.manager.ServiceManager;
 
 import java.net.URI;
@@ -34,11 +37,19 @@ public class Main {
         URL ontoUrl = new URL("http://localhost:" + port + "/wsc/ontology/ontology.owl");
         // Import data
         WSCImportUtils.importDataset(iserve, ontoUrl, WSCTest.TESTSET_2008_01, false);
-        // Create MatchGraph
+
+        System.out.println("Available matchers: " + iserve.listAvailableMatchers());
+        // Create a simple KB-Based MatchGraph that meets the WSC match rules (exact/plugin match)
         final MatchGraph<URI, LogicConceptMatchType> matchGraph = new iServePluginKBMatchGraph(ontoUrl.toURI(),
                 iserve.getRegistryManager().getKnowledgeBaseManager());
+
         final ServiceManager serviceManager = iserve.getRegistryManager().getServiceManager();
+        final ConceptMatcher iserveMatcher = iserve.getDefaultConceptMatcher();
+        final ConceptMatcher indexedMatcher = iserve.getConceptMatcher("uk.ac.open.kmi.iserve.discovery.disco.impl.SparqlIndexedLogicConceptMatcher");
         final OperationTranslator opManager = new iServeOperationTranslatorImpl(serviceManager);
+        final InputDiscoverer<URI> discoverer = new iServeMatchGraphBasedDiscoverer(opManager, serviceManager, matchGraph);
+        final InputDiscoverer<URI> iserveDiscoverer = new iServeOperationDiscovererAdapter(
+                new GenericLogicDiscoverer(serviceManager, indexedMatcher), opManager);
 
         CompositionProblem<URI, LogicConceptMatchType> problem = new CompositionProblem<URI, LogicConceptMatchType>() {
             @Override
@@ -48,7 +59,7 @@ public class Main {
 
             @Override
             public InputDiscoverer<URI> getInputDiscoverer() {
-                return new iServeMatchGraphBasedDiscoverer(opManager, serviceManager, matchGraph);
+                return iserveDiscoverer;
             }
         };
         ComposIT<URI, LogicConceptMatchType> composit = new ComposIT<URI, LogicConceptMatchType>(problem);
@@ -73,6 +84,6 @@ public class Main {
 
         composit.search(request);
 
-
+        iserve.shutdown();
     }
 }
---
+++ src/main/java/es/usc/citius/composit/iserve/discovery/iServeOperationDiscovererAdapter.java
@@ -0,0 +1,42 @@
+package es.usc.citius.composit.iserve.discovery;
+
+
+import es.usc.citius.composit.core.composition.InputDiscoverer;
+import es.usc.citius.composit.core.model.Operation;
+import es.usc.citius.composit.iserve.OperationTranslator;
+import uk.ac.open.kmi.iserve.discovery.api.MatchResult;
+import uk.ac.open.kmi.iserve.discovery.api.OperationDiscoverer;
+
+import java.net.URI;
+import java.util.*;
+
+public class iServeOperationDiscovererAdapter implements InputDiscoverer<URI> {
+    private OperationDiscoverer discoverer;
+    private OperationTranslator opManager;
+
+    public iServeOperationDiscovererAdapter(OperationDiscoverer discoverer, OperationTranslator opManager) {
+        this.discoverer = discoverer;
+        this.opManager = opManager;
+    }
+
+    @Override
+    public Set<Operation<URI>> findOperationsConsuming(URI input) {
+        Map<URI, MatchResult> result = discoverer.findOperationsConsumingSome(Collections.singleton(input));
+        // Apply filtering?
+        Set<Operation<URI>> ops = new HashSet<Operation<URI>>();
+        // Convert
+        for(URI operationUri : result.keySet()){
+            ops.add(opManager.getOperation(operationUri));
+        }
+        return ops;
+    }
+
+    @Override
+    public Set<Operation<URI>> findOperationsConsumingSome(Collection<URI> inputs) {
+        Set<Operation<URI>> ops = new HashSet<Operation<URI>>();
+        for(URI input : inputs){
+            ops.addAll(findOperationsConsuming(input));
+        }
+        return ops;
+    }
+}
